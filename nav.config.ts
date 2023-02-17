92ba00f8248e9f3ccc40957af3f3e6712e589193
wangyan <wangyanjava@gmail.com>
2023-02-17 13:44:34 +0800

    commit

--- nav.config.ts
+++ nav.config.ts
@@ -3,7 +3,7 @@ import { IConfig } from './src/types'
 const c: IConfig = {
   // [Mondatory], Please replace following Github url with your own Github address which you forked in.
   // [必填], 请填写您的仓库地址
-  gitRepoUrl: 'https://github.com/wang-yan-github/nav',
+  gitRepoUrl: 'https://gitee.com/wang-yan-gitee/nav',
 
   // Deployment branch name
   // 部署分支
